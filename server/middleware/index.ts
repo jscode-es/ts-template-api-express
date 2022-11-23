cfd3c5444359b1770cbc47693f2d631aca528ece
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-11-23 19:47:58 +0100

    add: middleware

---
+++ server/middleware/index.ts
@@ -0,0 +1,23 @@
+import { Response, NextFunction } from 'express'
+import { CustomRequest } from '../types'
+
+export const middleware = ( req: CustomRequest, res: Response, next: NextFunction ) =>
+{	
+	// Ignore favicon
+	if (req.originalUrl.includes('favicon.ico')) return res.status(204).end()
+
+	// Ip client
+	req.ipClient = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+	// Check is json
+	req.isJsonRequest = req.get('Content-Type') === 'application/json'
+
+	// Force convert method
+	req.method = String(req.method).toLowerCase()
+
+	// Focus unique data
+	req.data = Object.keys(req.query).length ? req.query : req.body
+
+	// Continue request
+	next()
+}	
