154cff9094beea6154f812d5045a6b4fab6d0e3f
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-11-23 20:26:00 +0100

    Update index.ts

--- server/types/index.ts
+++ server/types/index.ts
@@ -4,7 +4,7 @@ export interface IObject {
 	[key: string]: string | number | boolean
 }
 
-export interface CustomRequest extends Request {
+export interface IRequest extends Request {
 	isJsonRequest?: boolean;
 	ipClient?: string | string[];
 	data?: IObject;
@@ -15,4 +15,17 @@ export interface ICustomEnv extends NodeJS.ProcessEnv {
 	port?: string;
 	host?: string;
 	limit_json?: string;
+}
+
+export interface IReturn 
+{
+	status:number 
+	error?:string[]
+	result?:string|number|boolean|IObject
+}
+
+export interface IData 
+{
+	data:IObject
+	request:IRequest
 }
